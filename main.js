bf1a0f05af7412ec91fbc8b306473666c7636ff1
milux zangeneh <miluxhd@gmail.com>
2020-07-07 03:51:20 +0430

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function validate(username, password) {
-    if (username == null || password == null)
+    if (username === '' || password === '')
         return false;
     return true;
 }
@@ -11,18 +11,28 @@ function login() {
 
     if (!validate(username, password)) {
         console.log("invalid username or password");
+        alert("invalid username or password")
         return;
     }
+
     function callback() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 console.log("logging!!!!");
                 var result = xhr.responseText;
                 var obj = JSON.parse(result);
-                browser.storage.local.set({'prx_username': obj.prx_username});
-                browser.storage.local.set({'prx_password': obj.prx_password});
-                browser.storage.local.set({'prx_port': obj.prx_port});
-                browser.storage.local.set({'prx_host': obj.prx_host});
+                chrome.storage.local.set({
+                        'prx_username': obj.prx_username,
+                        'prx_password': obj.prx_password,
+                        'prx_port': obj.prx_port,
+                        'prx_host': obj.prx_host,
+                        'prx_pac': obj.prx_pac,
+                        'login_password': password,
+                        'login_username': username
+                    }
+                );
+                alert(obj.prx_pac);
+
             } else {
                 alert(xhr.status);
             }
@@ -30,7 +40,10 @@ function login() {
     };
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://thegates/credentials.json", true);
+    let url = new URL('https://panjreh.ir/login');
+    url.searchParams.set('u', username);
+    url.searchParams.set('p', password);
+    xhr.open("GET", url, true);
     xhr.onreadystatechange = callback;
     xhr.send();
 }
@@ -43,10 +56,10 @@ function toggle() {
         var x = document.getElementById("togglebtn");
         if (data.prx_mode === "enabled") {
             x.innerHTML = "Disabled";
-            browser.storage.local.set({'prx_mode': 'disabled'});
+            chrome.storage.local.set({'prx_mode': 'disabled'});
         } else {
             x.innerHTML = "Enabled";
-            browser.storage.local.set({'prx_mode': 'enabled'});
+            chrome.storage.local.set({'prx_mode': 'enabled'});
         }
     });
 
@@ -55,12 +68,17 @@ function toggle() {
 document.addEventListener('DOMContentLoaded', function () {
     var togglebtn = document.getElementById("togglebtn");
     var loginbtn = document.getElementById("loginbtn");
-    chrome.storage.local.get('prx_mode', function (data) {
-        if (data.prx_mode == 'enabled')
+
+    chrome.storage.local.get(['prx_mode', 'login_username', 'login_password'], function (data) {
+        if (data.login_username && data.login_password) {
+            document.getElementById("username").value = data.login_username;
+            document.getElementById("password").value = data.login_password;
+        }
+        if (data.prx_mode === 'enabled')
             togglebtn.innerHTML = "Enabled";
         else
             togglebtn.innerHTML = "Disabled";
     });
-    loginbtn.addEventListener('click', login);
     togglebtn.addEventListener('click', toggle);
+    loginbtn.addEventListener('click', login);
 });
--- proxy.js
+++ proxy.js
@@ -1,67 +1,94 @@
 let authData = {};
 
-chrome.webRequest.onAuthRequired.addListener(sendAuth, {urls: ['*://*/*']}, ['blocking']);
+function sendAuth(details, callbackFn) {
+        console.log("onAuthRequired!", details, callbackFn);
+        if (!Object.keys(authData)[0]) {
+            loadAuthData();
+        }
+        callbackFn({
+            authCredentials:{username: authData["prx_username"], password: authData["prx_password"]}
+        });
+    }
+
+chrome.webRequest.onAuthRequired.addListener(sendAuth,
+    {urls: ["<all_urls>"]},
+    ['asyncBlocking']
+);
+
 chrome.storage.onChanged.addListener(storageOnChanged);
 
 function storageOnChanged(changes, area) {
     Object.keys(changes).forEach(element => {
-        if (changes[element].newValue !== changes[element].oldValue) {
-            authData[element] = changes[element].newValue;
-            if (element === "prx_mode")
-                if (authData["prx_mode"] === "disabled")
-                    setDisabled();
-                else if (authData["prx_mode"] === "enabled")
-                    setEnabled();
-        }
+        // if (changes[element].newValue !== changes[element].oldValue) {
+        authData[element] = changes[element].newValue;
+        if (element === "prx_mode")
+            if (authData["prx_mode"] === "disabled")
+                setDisabled();
+            else if (authData["prx_mode"] === "enabled")
+                setEnabled();
+        // }
     });
 }
 
-browser.runtime.onInstalled.addListener(details => {
-    loadAuthData()
-});
+chrome.runtime.onInstalled.addListener(function callback() {
+    loadAuthData();
+})
 
-
-async function sendAuth(details, callbackFn) {
-    console.log("onAuthRequired!", details, callbackFn);
-    if (!Object.keys(authData)[0]) {
-        await loadAuthData();
-    }
-    return {authCredentials: {username: authData["prx_username"], password: authData["prx_password"]}}
-}
-
-async function loadAuthData() {
-    await new Promise(resolve => {
-        chrome.storage.local.get(['prx_username', 'prx_password', 'prx_host', 'prx_port', 'prx_mode'], function (data) {
+function loadAuthData() {
+        chrome.storage.local.get(['prx_username', 'prx_password', 'prx_host', 'prx_port', 'prx_mode', 'prx_pac'], function (data) {
             authData['prx_username'] = data.prx_username;
             authData['prx_password'] = data.prx_password;
             authData['prx_host'] = data.prx_host;
             authData['prx_port'] = data.prx_port;
             authData['prx_mode'] = data.prx_mode;
-            resolve();
-        });
+            authData['prx_pac'] = data.prx_pac;
     });
 }
 
+function setEnabled() {
+    setMode(true);
+    setProxy();
+    console.log('******* enabled mode');
+    console.log(authData);
+}
 
-async function proxyRequest(requestInfo) {
+function setDisabled() {
+    setMode(false);
+    setProxy();
+    console.log('******* disabled mode');
+    console.log(authData);
+}
+
+async function setProxy() {
+    var config = null;
+
     if (!Object.keys(authData)[0]) {
         await loadAuthData();
     }
     console.log(authData);
-    const ret = {
-        type: 'http',
-        host: authData['prx_host'],
-        port: authData['prx_port']
-    };
-    return ret;
+
+    if (authData['prx_mode'] === "enabled")
+        config = {
+            mode: "pac_script",
+            pacScript: {
+                url: authData['prx_pac']
+            }
+        };
+    else
+        config = {
+            mode: "system"
+        };
+
+    chrome.proxy.settings.set({
+        value: config,
+        scope: 'regular'
+    }, function () {
+    });
 }
 
-function setEnabled() {
-    browser.proxy.onRequest.addListener(proxyRequest, {urls: ["https://www.google.com/*", "*://ifconfig.co/*" , "*://*.chrome.com/*"]});
-}
-
-function setDisabled() {
-    browser.proxy.onRequest.hasListener(proxyRequest) && browser.proxy.onRequest.removeListener(proxyRequest);
-    console.log('******* disabled mode');
-    console.log(authData);
+function setMode(status) {
+    if (status)
+        authData['prx_mode'] = 'enabled';
+    else
+        authData['prx_mode'] = 'disabled';
 }
